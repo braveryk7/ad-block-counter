59e24b4142242ae757b0ead21429b9c35d3847ab
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-15 17:34:27 +0900

    refs #5 initial commit

---
+++ class/class-rinker-class-name.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Rewrite the ClassName of Rinker.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Ad Block Counter
+ * @since      0.1.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
