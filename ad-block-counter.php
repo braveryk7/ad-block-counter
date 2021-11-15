4973b319955f6cf3fb6a2bc46f1a4a9921282e1f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-15 17:18:38 +0900

    refs #3 add Ad Block Counter base

--- ad-block-counter.php
+++ ad-block-counter.php
@@ -1,19 +1,26 @@
 <?php
 /**
- * Plugin Name:       WordPress Plugin Template
+ * Plugin Name:       Ad Block Counter
  * Description:       Example block written with ESNext standard and JSX support – build step required.
- * Requires at least: 5.8
- * Requires PHP:      7.0
+ * Requires at least: 5.8.2
+ * Requires PHP:      7.3
  * Version:           0.1.0
  * Author:            Ken-chan
  * Author URI:        https://twitter.com/braveryk7
  * Plugin URI:        https://www.braveryk7.com/
- * Text Domain:       wordpress-plugin-template
+ * Text Domain:       ad-block-counter
  * Domain Path:       /languages
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @author            Ken-chan
- * @package           create-block
+ * @package           WordPress
+ * @subpackage        Ad Block Counter
  * @since             0.1.0
  */
+
+declare(strict_types = 1);
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
