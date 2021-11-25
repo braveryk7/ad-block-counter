f738e6497822f9fb7abcbffb10cc1a4bf4fcccee
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-26 02:17:27 +0900

    refs #6 add import scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
+import './scss/index.scss';
 import { Toggle } from './component/molecules/Toggle';
 
 const AdminPage = () => {
