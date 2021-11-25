c3a12033524286796abce99cfbde9a8ecbd51de9
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-26 01:14:01 +0900

    refs #6 add settings link, page contents

---
+++ class/class-abc-settings-page.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Admin settings page.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Ad Block Counter
+ * @since      0.1.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Admin settings page.
+ */
+class Abc_Settings_Page {
+	/**
+	 * Add Ad Block Counter to admin bar.
+	 */
+	public function add_menu() {
+		add_options_page(
+			__( 'Ad Block Counter', 'ad-block-counter' ),
+			__( 'Ad Block Counter', 'ad-block-counter' ),
+			'administrator',
+			'ad-block-counter',
+			[ $this, 'abc_settings_page' ]
+		);
+	}
+
+	/**
+	 * Settings page.
+	 */
+	public function abc_settings_page() {
+		echo '<div id="ad-block-counter-settings"></div>';
+	}
+}
