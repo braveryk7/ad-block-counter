85091eff915e00be0f2742c8c9d4ea614fdd0aba
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-26 01:47:03 +0900

    refs #6 add Toggle component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { render } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
+import { Toggle } from './component/molecules/Toggle';
+
 const AdminPage = () => {
 	return (
 		<div id="wrap">
 			<h1>{ __( 'Ad Block Counter Settings', 'ad-block-counter' ) }</h1>
+			<Toggle />
 		</div>
 	);
 };
