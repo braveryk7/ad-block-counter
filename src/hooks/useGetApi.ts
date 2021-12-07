58233e9c72d2130c1b95a2ba4029f7e4fba4596b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-12-07 22:37:54 +0900

    refs #6 initial commit

---
+++ src/hooks/useGetApi.ts
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction, useEffect } from 'react';
+
+// @ts-ignore
+import api from '@wordpress/api'; // eslint-disable-line
+import { apiType, WPApiType } from '../types/apiType';
+
+export const useGetApi = (
+	stateFunc: Dispatch< SetStateAction< apiType > >,
+	setApiStatus: Dispatch< SetStateAction< boolean > >
+) => {
+	useEffect( () => {
+		api.loadPromise.then( () => {
+			const model = new api.models.Settings();
+
+			model.fetch().then( ( res: WPApiType< any > ) => {
+				stateFunc( res );
+				setApiStatus( true );
+			} );
+		} );
+	}, [ stateFunc ] );
+};
