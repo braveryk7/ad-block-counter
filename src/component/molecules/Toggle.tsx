b4572e93374e1bfe5d314b7f34c74033eecfb766
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-01-16 18:55:30 +0900

    refs #6 add create class name => future...

--- src/component/molecules/Toggle.tsx
+++ src/component/molecules/Toggle.tsx
@@ -4,6 +4,8 @@ import { useContext } from '@wordpress/element';
 import { apiContext } from '../..';
 import { useSetApi } from '../../hooks/useSetApi';
 import { apiType } from '../../types/apiType';
+// import { rinkerClasses } from '../../utils/RinkerClassName';
+// import { createClassName } from '../../utils/createClassName';
 
 export const Toggle = ( props: any ) => {
 	const { itemKey, label } = props;
@@ -16,7 +18,20 @@ export const Toggle = ( props: any ) => {
 		setApiData( newItem );
 	};
 
+	// if ( apiData.abc_rinker && ! apiData.abc_rinker_classes ) {
+	// 	const newRinkerClasses: any = {};
+	// 	rinkerClasses.map( ( rinkerClass ) => {
+	// 		return ( newRinkerClasses[ rinkerClass ] = createClassName() );
+	// 	} );
+
+	// 	const newItem: apiType = JSON.parse( JSON.stringify( { ...apiData } ) );
+
+	// 	newItem.abc_rinker_classes = newRinkerClasses;
+	// 	setApiData( newItem );
+	// }
+
 	useSetApi( itemKey, apiData.abc_rinker! );
+	// useSetApi( 'abc_rinker_classes', apiData.abc_rinker_classes! );
 
 	return (
 		<ToggleControl
