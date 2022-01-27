f7f290feee796ed1090a02af387f056e0b16450d
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-01-27 18:08:46 +0900

    refs #14 add CodeEditor component

---
+++ src/component/molecules/CodeEditor.tsx
@@ -0,0 +1,22 @@
+import { css } from '@codemirror/lang-css';
+import CodeMirror from '@uiw/react-codemirror';
+
+import { CssEditorPropsType } from '../../types/ItemType';
+
+export const CodeEditor = ( props: CssEditorPropsType ) => {
+	const { editorValue, setEditorValue } = props;
+
+	return (
+		<CodeMirror
+			value={ editorValue }
+			width="80%"
+			height="400px"
+			min-width="50%"
+			min-height="400px"
+			max-height="500px"
+			theme="light"
+			extensions={ [ css() ] }
+			onChange={ ( value ) => setEditorValue!( value ) }
+		/>
+	);
+};
