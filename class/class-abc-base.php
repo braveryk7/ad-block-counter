7b909f2fdd56b57a4dc91e922e8d938088f0a788
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-01-30 17:24:38 +0900

    refs #23 add class base

--- class/class-abc-base.php
+++ class/class-abc-base.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * Ad Block Counter base class.
+ *
+ * @author     Ken-chan
+ * @package    WordPress
+ * @subpackage Ad Block Counter
+ * @since      0.1.0
+ */
 
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Ad Block Counter base class.
+ */
+class Abc_Base {
+}
