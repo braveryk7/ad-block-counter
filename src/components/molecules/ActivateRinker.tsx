8bc04552cdf343dcf906d7cab47c17c2d3ba0660
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-01 00:58:04 +0900

    refs #26 fix rinker installed or activate components

--- src/components/molecules/ActivateRinker.tsx
+++ src/components/molecules/ActivateRinker.tsx
@@ -2,24 +2,50 @@ import { __ } from '@wordpress/i18n';
 
 import { addPrefix } from '../../utils/constant';
 
-export const ActivateRinker = () => {
+export const ActivateRinker = ( props: { rinkerStatus: boolean } ) => {
+	const disabledTitle = __(
+		'This plugin requires to Rinker enabled.',
+		'ad-block-counter'
+	);
+	const disabledMessage = [
+		__( 'Current Rinker status: ', 'ad-block-counter' ) +
+			__( 'Disabled', 'ad-block-counter' ),
+		__(
+			'Rinker to be enabled on the Installed Plugins page.',
+			'ad-block-counter'
+		),
+	];
+	const uninstalledTitle = __(
+		'This plugin requires to Rinker installed.',
+		'ad-block-counter'
+	);
+	const uninstalledMessage = [
+		__( 'Current Rinker status: ', 'ad-block-counter' ) +
+			__( 'Uninstalled', 'ad-block-counter' ),
+		__(
+			'Rinker is a product link management plugin that is completely free to use.',
+			'ad-block-counter'
+		),
+	];
+
 	return (
 		<div className={ `${ addPrefix( '-item-wrapper' ) }` }>
-			<h2>
-				{ __(
-					'This plugin requires to Rinker enabled.',
-					'ad-block-counter'
-				) }
-			</h2>
+			<h2>{ props.rinkerStatus ? disabledTitle : uninstalledTitle }</h2>
+			{ props.rinkerStatus
+				? disabledMessage.map( ( value, i ) => (
+						<p key={ i }>{ value }</p>
+				  ) )
+				: uninstalledMessage.map( ( value, i ) => (
+						<p key={ i }>{ value }</p>
+				  ) ) }
 			<p>
-				{ __( 'Current Rinker status', 'ad-block-counter' ) }:{ ' ' }
-				{ __( 'Disabled', 'ad-block-counter' ) }
-			</p>
-			<p>
-				{ __(
-					'Rinker to be enabled on the Installed Plugins page.',
-					'ad-block-counter'
-				) }
+				<a
+					href="https://oyakosodate.com/rinker/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{ __( 'Rinker Official web site', 'ad-block-counter' ) }
+				</a>
 			</p>
 		</div>
 	);
