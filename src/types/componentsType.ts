adfca53ac15b1b765a2ac126ba6f27234cfbe78b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-01-31 15:26:15 +0900

    refs #26 fix move CssEditorPropsType is ItemType.ts->componentsType.ts

--- src/types/componentsType.ts
+++ src/types/componentsType.ts
@@ -2,3 +2,9 @@ export type TogglePropsType = {
 	itemKey: 'abc_rinker' | 'abc_logged_in_user';
 	label: string;
 };
+
+export type CssEditorPropsType = {
+	itemKey?: string;
+	editorValue: string;
+	setEditorValue?: React.Dispatch< React.SetStateAction< string > >;
+};
