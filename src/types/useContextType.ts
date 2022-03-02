cbbdb444c3841c2db57fff7b2a265e6973b56db5
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-02 22:37:56 +0900

    refs #48 fix snackbarTimer type

--- src/types/useContextType.ts
+++ src/types/useContextType.ts
@@ -14,5 +14,5 @@ export type apiContextType = {
 	setNoticeStatus: Dispatch< SetStateAction< boolean > >;
 	setNoticeValue: Dispatch< SetStateAction< noticeValueType > >;
 	setNoticeMessage: Dispatch< SetStateAction< string > >;
-	snackbarTimer: NodeJS.Timeout;
+	snackbarTimer: number;
 };
